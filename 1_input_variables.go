63655f5a55a984560461446a98875144cb046be5
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-28 19:56:28 -0700

    [fix] refactor to one file, two programs

---
+++ 1_input_variables.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 1. Write a program that calculates the birth year using a provided date of birth and age. HINT: Get the date of birth and age from stdin!
+// 2. Write a program that calculates the average weight of 5 people.
+
+func main() {
+	fmt.Println("Hello, \nWhat day, month and year were you born?")
+}
