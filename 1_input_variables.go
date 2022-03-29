5d534df96daf29a733fb21e2a8f9d3acb2b5b1a1
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-28 23:00:46 -0700

    [add] return format strings

--- 1_input_variables.go
+++ 1_input_variables.go
@@ -10,44 +10,48 @@ import (
 // 1. Write a program that calculates the birth year using a provided date of birth and age. HINT: Get the date of birth and age from stdin!
 // 2. Write a program that calculates the average weight of 5 people.
 
-var (
-	day   int
-	month int
-	year  int
-)
+// var (
+// 	day   int
+// 	month int
+// 	year  int
+// )
 
-const (
-	jan = iota + 1
-	feb
-	mar
-	apr
-	may
-	jun
-	jul
-	aug
-	sep
-	oct
-	nov
-	dec
-)
+// const (
+// 	jan = iota + 1
+// 	feb
+// 	mar
+// 	apr
+// 	may
+// 	jun
+// 	jul
+// 	aug
+// 	sep
+// 	oct
+// 	nov
+// 	dec
+// )
 
-const (
-	january = iota + 1
-	february
-	march
-	april
-	june
-	july
-	august
-	september
-	october
-	november
-	december
-)
+// const (
+// 	january = iota + 1
+// 	february
+// 	march
+// 	april
+// 	june
+// 	july
+// 	august
+// 	september
+// 	october
+// 	november
+// 	december
+// )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Hello, \nWhat day, month and year were you born?\nFormat: ##/##/####")
-	date, _ := reader.ReadString('\n')
-	fmt.Print("Your birthdate is " + date)
+	fmt.Println("Hello, \nWhat year were you born in?")
+	year, _ := reader.ReadString('\n')
+	fmt.Println("What month were you born in?")
+	month, _ := reader.ReadString('\n')
+	fmt.Printf("What day of %s", month)
+	day, _ := reader.ReadString('\n')
+	fmt.Printf("Your birthdate is %s %s %s", day, month, year)
 }
