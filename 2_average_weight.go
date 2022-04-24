27e400feb840b266b8011fb01d69afe5146c5545
CHRIS_LEE <chrismlee87@gmail.com>
2022-04-23 23:15:16 -0700

    [fix] slight refactor -- begin removing nested for loops

--- 2_average_weight.go
+++ 2_average_weight.go
@@ -21,25 +21,23 @@ func main() {
 		// Convert string array to int array
 		input_weight_array_int := make([]int, len(input_weight_array))
 
+		average_weight := 0
+		// convert results to int
+		// weight_array_int, _ := strconv.ParseInt(input_weight_array, 10, 64)
+
 		for j := 0; j < len(input_weight_array); j++ {
 			input_weight_array_int[j], _ = strconv.Atoi(input_weight_array[j])
 		}
 
 		// Calculate average weight
-		average_weight := 0
 		for j := 0; j < len(input_weight_array_int); j++ {
 			average_weight += input_weight_array_int[j]
 		}
+
+		// Calculate average
 		average_weight = average_weight / len(input_weight_array_int)
 		fmt.Println("Average weight: ", average_weight)
 
-		// Calculate average
-		// average_weight := (input_weight_array[i] + input_weight_array[i+1] + input_weight_array[i+2] + input_weight_array[i+3] + input_weight_array[i+4]) / 5
-
-		// fmt.Println("Average weight: ", average_weight)
 	}
-
-	// weight_array_int, _ := strconv.ParseInt(input_weight_array, 10, 64)
-	// fmt.Println("You entered: ", weight_array_int)
 
 }
